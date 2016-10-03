bc1149ec04caf9c00be47ffcd4437c3675aee78b
Bruno Portable <galliez.bruno@gmail.com>
2016-10-03 16:47:47 +0200

    Ex8 ++

--- ex8/ex8/main.cpp
+++ ex8/ex8/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <math.h>
+
 #include "main.h"
+
 using namespace std;
-int saisirParam(){
+
+
+// a)
+int saisirParametres ()
+{
 
 	int rayon;
 
@@ -12,7 +19,11 @@ int saisirParam(){
 
 }
 
-void saisirParam(int *largeur, int *longueur){
+
+
+// b)
+void saisirParametres ( int *largeur, int *longueur )
+{
 	int la,lo;
 	cout << "Veuillez introduire la largeur ( int )" << endl;
 	cin >> la;
@@ -23,6 +34,53 @@ void saisirParam(int *largeur, int *longueur){
 	*longueur = lo;
 }
 
-void main(){
+
+
+// c)
+void saisirParametres ( int *longueurUne, int *longueurDeux, float *angle )
+{
+	int la,lo;
+	float an;
+	cout << "Veuillez introduire la premiere longueur ( int )" << endl;
+	cin >> la;
+	cout << "veuillez introduire la deuxieme longueur ( int ) " << endl;
+	cin >> lo;
+	cout << "veuillez introduire la valeur de l'angle ( int ) " << endl;
+	cin >> an;
+
+	*longueurUne = la;
+	*longueurDeux = lo;
+	*angle = an;
+}
+
+
+
+// d)
+float afficherSurface ( float rayon = 2 )
+{
+	cout << " Surface cercle : " << (3.14 * rayon * rayon ) << endl;
+}
+
+
+
+// e)
+float afficherSurface ( float largeur, float longueur )
+{
+	cout << " Surface rectangle : " << ( largeur * longueur ) << endl;
+}
+
+
+
+// f)
+float afficherSurface ( float coteUn, float coteDeux, float angle )
+{
+	cout << " Surface rectangle : " << ( (coteUn * coteDeux)/2 * sin(angle) ) << endl;
+}
+
+
+
+// g)
+void main (int arc, char *argv[])
+{
 
 }
--- ex8/ex8/main.h
+++ ex8/ex8/main.h
@@ -1,2 +1,7 @@
-int saisirParam();
-void saisirParam(int *largeur, int *longueur);
+int saisirParametres ();
+void saisirParametres ( int *largeur, int *longueur );
+void saisirParametres ( int *longueurUne, int *longueurDeux, float *angle );
+float afficherSurface ( float rayon = 2 );
+float afficherSurface ( float largeur, float longueur );
+float afficherSurface ( float coteUn, float coteDeux, float angle )
+
